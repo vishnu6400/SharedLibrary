58c33c88ada5596a761173872578f888968f55fe
vishnu6400 <131434435+vishnu6400@users.noreply.github.com>
2023-05-02 11:13:11 +0530

    Update math.groovy

--- var/math.groovy
+++ var/math.groovy
@@ -1,19 +1,19 @@
 def sum(a,b)
 {
- "that sum is $(a) and $(b) is $(a+b)"
+ echo "that sum is $(a) and $(b) is $(a+b)"
 }
 
 def sub(a,b)
 {
-   "that sub is $(a) and $(b) is $(a-b)"
+  echo  "that sub is $(a) and $(b) is $(a-b)"
 }
 
 def mul(a,b)
 {
-  "that product is $(a) and $(b) is $(a*b)"
+ echo  "that product is $(a) and $(b) is $(a*b)"
 }
 
 def dev(a,b)
 {
-  "that devision is $(a) and $(b) is $(a/b)"
+  echo "that devision is $(a) and $(b) is $(a/b)"
 }
